2c1e28e9e550b5010a70c195e5b613f98347ebd5
Mrakorez <66641112+Mrakorez@users.noreply.github.com>
2026-03-13 12:26:51 +0200

    fix(install): preserve executable extension in hardlinks

--- src/install.rs
+++ src/install.rs
@@ -1,6 +1,6 @@
 use std::env::args_os;
 use std::fs::hard_link;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use clap::Args;
 
@@ -18,9 +18,11 @@ impl Cmd {
         let Some(prog) = args_os().next() else {
             return Err(Error::Executable);
         };
+        let ext = Path::new(&prog).extension().unwrap_or_default();
 
         for subcmd in ["sdat2img", "img2sdat"] {
-            let target = self.dir.join(subcmd);
+            let mut target = self.dir.join(subcmd);
+            target.set_extension(ext);
             hard_link(&prog, &target).map_err(|e| Error::Io(target, e))?;
         }
 
